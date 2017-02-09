250c38ad952cfa1a078a82009ea8ae9aa42bf534
LB <lbg232@gmail.com>
2017-02-09 17:56:29 +0100

    Create Eva.c

---
+++ Eva.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//#
+// -Guardar todo en un array
+// -Imprimir array.
+//#
+
+int main()
+{
+    int c;
+    int index;
+    int arr[100];
+    index = 0;
+    
+    while((c = getchar()) != EOF)
+    {   
+        
+			putchar(c);
+		
+	}
+		
+}
